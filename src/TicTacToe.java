10bc57b76d3434a6dce7b284cf7ab71a3f5527bf
Izad Khokhar <72565064+IK-49@users.noreply.github.com>
2024-04-26 04:37:49 +0000

    - core functionality complete: game win state with diagonal, row, and columns, began work on point system, player turns

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class TicTacToe 
 {
-    static int[][] gameBoard = new int[3][3];
+    static String[][] gameBoard = new String[3][3];
+    static boolean isPlayer1Turn = true;
+    static String playersCurrentLetter;
+    static int player1Score = 0;
+    static int player2Score = 0;
 
     public static void main(String[] args)
     {        
@@ -21,25 +25,33 @@ public class TicTacToe
         if(decision.equals("PLAY"))
         {
             initializeGameBoard();
-            displayGameBoard();
+            updateGameState();
+            scan.close();
         }
-        else if(decision.equals("PLAY")) 
+        else if(decision.equals("EXIT")) 
         {
-            System.exit(0);
+            scan.close();
+            System.exit(0);            
         }
         else 
         {
             System.out.println("Invalid Input!");
+            scan.close();
+            System.exit(0);  // TO-DO: add exception handling          
         }
+
+        scan.close();
     }
 
     static void initializeGameBoard() 
     {
+        int z = 1;
         for(int i = 0; i < 3; i++) 
         {
             for(int j = 0; j < 3; j++) 
             {
-                gameBoard[i][j] = 0;
+                gameBoard[i][j] = Integer.toString(z);
+                z += 1;
             }
         }
     }
@@ -53,25 +65,60 @@ public class TicTacToe
         {
             for(int j = 0; j < 3; j++) 
             {
-                System.out.print(gameBoard[i][j] + "\t\t\t\t\t");
+                System.out.print(gameBoard[i][j] + " \t\t | \t\t");
             }
             
-            System.out.println("\n\n\n");
+            System.out.println("\n");
+            System.out.println("----------------------------------------------------------------------------------"); // find more efficient method to implement this
+            System.out.println("\n");
         }
     }
 
-    static boolean isGameOver() 
+    static boolean gameIsOver() 
     {
-        return true;
+        // if 3 in a row, col, or diagonal of the same character then game over and print game winner
+        
+        // diagonal check
+        if(gameBoard[0][0] == playersCurrentLetter && (gameBoard[0][0].equals(gameBoard[1][1]) && gameBoard[0][0].equals(gameBoard[2][2])))
+        {
+            return true;    
+        }
+        // anti-diagonal check
+        if(gameBoard[0][2] == playersCurrentLetter && (gameBoard[0][2].equals(gameBoard[1][1]) && gameBoard[0][2].equals(gameBoard[2][0])))
+        {
+            return true;
+        }
+
+        // row check
+        for(int row = 0; row < 3; row++) 
+        {
+            if(gameBoard[row][0] == gameBoard[row][1] && gameBoard[row][1] == gameBoard[row][2]) 
+            {
+                return true;
+            }
+        }
+
+        // column check
+        for(int col = 0; col < 3; col++) 
+        {
+            if(gameBoard[0][col] == gameBoard[1][col] && gameBoard[1][col] == gameBoard[2][col]) 
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     static void updateGameState() 
     {
         // clear screen
+        clearConsole();
+        // display the board
+        displayGameBoard();
 
-        // change selected slot value to either x or o depending on which player's turn it is.
+        playerTurn();
 
-        // display board again
     }
 
     static void displayMessage(String message) 
@@ -96,5 +143,84 @@ public class TicTacToe
         {
 
         }
+    }
+
+    static void playerTurn() 
+    {
+        displayMessage("Select a number 1-9");
+        Scanner scan = new Scanner(System.in);
+        int playerChoice = scan.nextInt();
+        
+        if(isPlayer1Turn) 
+        {
+            playersCurrentLetter = "X";
+        }
+        else if(isPlayer1Turn == false) // when player 2 turn 
+        {
+            playersCurrentLetter = "O";
+        }
+
+        switch(playerChoice) 
+        {
+            case 1:
+                gameBoard[0][0] = playersCurrentLetter;
+                break;
+            case 2:
+                gameBoard[0][1] = playersCurrentLetter;
+                break;
+            case 3:
+                gameBoard[0][2] = playersCurrentLetter;
+                break;
+            case 4:
+                gameBoard[1][0] = playersCurrentLetter;
+                break;
+            case 5:
+                gameBoard[1][1] = playersCurrentLetter;
+                break;
+            case 6:
+                gameBoard[1][2] = playersCurrentLetter;
+                break;
+            case 7:
+                gameBoard[2][0] = playersCurrentLetter;
+                break;
+            case 8:
+                gameBoard[2][1] = playersCurrentLetter;
+                break;
+            case 9:
+                gameBoard[2][2] = playersCurrentLetter;
+                break;
+        }
+
+        if(gameIsOver()) 
+        {             
+            if(isPlayer1Turn) 
+            {
+                clearConsole();
+                displayGameBoard();
+
+                displayMessage("Player 1 Wins!");
+
+                // add one point for player 1
+                player1Score += 1;
+                displayMessage("Player 1's Score is: " + Integer.toString(player1Score));
+                System.exit(0);
+            }
+            else 
+            {
+                clearConsole();
+                displayGameBoard();
+
+                displayMessage("Player 2 Wins!");
+
+                // add one point for player 1
+                player2Score += 1;
+                displayMessage("Player 2's Score is: " + Integer.toString(player2Score));
+                System.exit(0);
+            }
+        }
+
+        isPlayer1Turn = !isPlayer1Turn;
+
+        updateGameState();
     }
 }
